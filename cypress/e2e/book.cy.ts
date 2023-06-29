b7e4f2f91efaefcb0de469d7c82ac66b999534f8
Martina Kraus <kraus.martina.m@gmail.com>
2023-06-29 13:59:45 +0200

    Solution Task23: clean up

--- cypress/e2e/book.cy.ts
+++ cypress/e2e/book.cy.ts
@@ -1,37 +1,46 @@
 describe('template spec', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:4200/books');
+    let ISBN: number;
+    beforeEach(() => {
+        cy.visit('http://localhost:4200/books');
 
-    cy.get('.books-grid').children().as('books');
-  });
+        cy.get('.books-grid').children().as('books');
+    });
 
-  it('passes', () => {
-    cy.get('[data-testid="app-title"]').contains('BOOK');
-  })
+    it('passes', () => {
+        cy.get('[data-testid="app-title"]').contains('BOOK');
+    })
 
-  it('should increase the number of books by 1', () => {
+    it('should increase the number of books by 1', () => {
 
-    let countBefore = 0;
+        let countBefore = 0;
 
-      cy.get('@books')
-        .then(books => (countBefore = books.length))
-        .then(() => {
-          const randomISBN = Math.floor(1000000000000 + Math.random() * 900000);
+        cy.get('@books')
+            .then(books => (countBefore = books.length))
+            .then(() => {
+                const randomISBN = Math.floor(1000000000000 + Math.random() * 900000);
 
-          cy.visit('http://localhost:4200/books/new');
-          cy.get('[data-test="isbn-field"]').type(String(randomISBN));
-          cy.get('[data-test="title-field"]').type('Lord of the rings');
-          cy.get('[data-test="author-field"]').type('Max Mustermann');
+                ISBN = randomISBN;
 
-          cy.get('[data-test="create-button"]').click();
+                cy.visit('http://localhost:4200/books/new');
+                cy.get('[data-test="isbn-field"]').type(String(randomISBN));
+                cy.get('[data-test="title-field"]').type('Lord of the rings');
+                cy.get('[data-test="author-field"]').type('Max Mustermann');
+
+                cy.get('[data-test="create-button"]').click();
+            })
+
+        cy.get('@books').then(books => {
+            console.log("countBefore", countBefore);
+            console.log("books.length", books.length);
+
+            expect(books.length).to.eq(countBefore + 1);
+
         })
+    })
 
-      cy.get('@books').then(books => {
-          console.log("countBefore", countBefore);
-          console.log("books.length", books.length);
-
-          expect(books.length).to.eq(countBefore + 1);
-
-      })
-  })
+    afterEach(() => {
+        if (ISBN) {
+            cy.request('DELETE', `http://localhost:4730/books/${ISBN}`);
+        }
+    })
 })
