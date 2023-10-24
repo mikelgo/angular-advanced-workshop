91fb530732bf748b74ada52b55855f351d150a3d
Michael Berger <m.gonser@icloud.com>
2023-10-24 08:50:00 +0200

    add spectator

---
+++ src/app/book/book-card/book-card.component.spectator.spec.ts
@@ -0,0 +1,48 @@
+import { MatCardModule } from '@angular/material/card';
+import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { Book, bookNa } from '../models';
+import { BookCardComponent } from './book-card.component';
+
+describe('<ws-book-card>', () => {
+    describe('unit', () => {
+        describe('When no content is passed', () => {
+            it('defaults to "n/a"', () => {
+                const na = 'n/a';
+                const component = new BookCardComponent();
+
+                expect(component.content.title).toBe(na);
+                expect(component.content.subtitle).toBe(na);
+                expect(component.content.author).toBe(na);
+            });
+        });
+    });
+
+    describe('template', () => {
+        let spectator: Spectator<BookCardComponent>;
+        let book: Book;
+
+        const createComponent = createComponentFactory({
+            component: BookCardComponent,
+            imports: [MatCardModule, RouterTestingModule]
+        });
+
+        beforeEach(() => {
+            spectator = createComponent();
+            book = { ...bookNa(), title: 'My book' };
+        });
+
+        describe('When content is passed', () => {
+            it('renders the content', () => {
+                spectator.setInput({ content: book });
+                expect(spectator.query('[data-test=book-card-title]')?.innerHTML).toContain(book.title);
+            });
+        });
+
+        describe('When no content is passed', () => {
+            it('renders "n/a"', () => {
+                expect(spectator.query('[data-test=book-card-title]')?.innerHTML).toContain('n/a');
+            });
+        });
+    });
+});
---
+++ src/app/book/book-new/book-new.component.spectator.spec.ts
@@ -0,0 +1,39 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { BookNewComponent } from './book-new.component';
+
+describe('<ws-book-new>', () => {
+    const matInputSelector = '[data-test=isbn-field] input';
+
+    let spectator: Spectator<BookNewComponent>;
+
+    const createComponent = createComponentFactory({
+        component: BookNewComponent,
+        imports: [
+            NoopAnimationsModule,
+            ReactiveFormsModule,
+            MatInputModule,
+            MatFormFieldModule,
+            MatButtonModule,
+            RouterTestingModule,
+            HttpClientTestingModule
+        ]
+    });
+
+    beforeEach(() => (spectator = createComponent()));
+
+    describe('When an ISBN has less than 3 characters', () => {
+        it('displays an error message', async () => {
+            spectator.typeInElement('12', matInputSelector);
+            spectator.blur(matInputSelector);
+
+            expect(spectator.queryAll('mat-error')).toContainText('ISBN has to be at least 3 characters long.');
+        });
+    });
+});
